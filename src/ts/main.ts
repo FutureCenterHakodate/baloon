d167c40ac2322f3879c5f72fdbf3c7633d8328fd
sKawashima <bysk.333@gmail.com>
2017-10-28 16:33:11 +0900

    クラスの定義（メソッド以外）作った

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,7 +3,7 @@ canvas.width = 800;
 canvas.height = 600;
 const ctx = canvas.getContext('2d');
 ctx.beginPath();
-ctx.fillStyle = "#E2E3E3";
+ctx.fillStyle = '#E2E3E3';
 ctx.rect(0,0,canvas.width,canvas.height);
 ctx.fill();
 
@@ -11,5 +11,16 @@ class Baloon{
   private x: number;
   private y: number;
   private shape: string;
-  
+  constructor(){
+    this.x = Math.random() * canvas.width;
+    this.y = canvas.height;
+    let i = Math.floor(Math.random()*3);
+    if(i == 0){
+      this.shape = '../img/shapeBaloon-ellipse.svg';
+    }else if(i == 1){
+      this.shape = '../img/shapeBaloon-hurt.svg';
+    }else{
+      this.shape = '../img/shapeBaloon-star.svg';
+    }
+  }
 }
