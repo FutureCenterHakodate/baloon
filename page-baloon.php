76eb75a3e4b69674a831b7ab33903c7e1a997467
ryu-g <b1013178@fun.ac.jp>
2017-10-28 15:53:15 +0900

    stand by
    仲仕込みの準備OKです

--- page-baloon.php
+++ page-baloon.php
@@ -1,11 +1,19 @@
+<?php
+/*
+Template Name: 固定ページの新規テンプレート１
+*/
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-	<meta charset="UTF-8">
-	<title><?php wp_title("- はこだてみらい館",true,'right');?></title>
+	<title><?php wp_title("- はこだてみらい館",true,'right');?></title>	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href=<?php echo get_template_directory_uri();?>/css/baloon.css>
+	<link rel="icon" href=<?php echo get_template_directory_uri();?>/img/favicon.ico>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
 </head>
 <body>
-	
+<iframe src="https://www.google.com/maps/d/embed?mid=1mUylVVj0RMzRIvoalkNCDq93P-M" width="100%" height="400"></iframe>
+
 </body>
 </html>
