44b84ef1d4dc88b8c577d40c27bc2002e9e42000
ryu-g <b1013178@fun.ac.jp>
2017-10-28 13:43:36 +0900

    remake cloud and add php file for templete

---
+++ page-baloon.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="UTF-8">
+	<title><?php wp_title("- はこだてみらい館",true,'right');?></title>
+</head>
+<body>
+	
+</body>
+</html>
