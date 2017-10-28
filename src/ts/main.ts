fc085122d1ce7c52b11c37a5736cdfb82d67984e
sKawashima <bysk.333@gmail.com>
2017-10-28 23:31:28 +0900

    微調整+atom-beauty

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,38 +5,37 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 ctx.beginPath();
 ctx.fillStyle = '#E2E3E3';
-ctx.rect(0,0,canvas.width,canvas.height);
+ctx.rect(0, 0, canvas.width, canvas.height);
 ctx.fill();
 
 const img0 = new Image();
 const img1 = new Image();
 const img2 = new Image();
-img0.src='../img/shapeBaloon-ellipse.svg';
-img1.src='../img/shapeBaloon-hurt.svg';
-img2.src='../img/shapeBaloon-star.svg';
+img0.src = '../img/shapeBaloon-ellipse.svg';
+img1.src = '../img/shapeBaloon-hurt.svg';
+img2.src = '../img/shapeBaloon-star.svg';
 
-class Baloon{
+class Baloon {
   private x: number;
   private y: number;
   private vy: number;
   private size: number;
   private shape: number;
-  constructor(){
+  constructor() {
     this.x = Math.random() * canvas.width;
     this.y = canvas.height;
-    this.vy = Math.random()*10;
-    this.size = Math.random()*100 + 50;
-    this.shape = Math.floor(Math.random()*3);
+    this.vy = Math.random() * 7;
+    this.size = Math.random() * 100 + 50;
+    this.shape = Math.floor(Math.random() * 3);
   }
-  move(){
+  move() {
     this.y -= this.vy;
   }
-  draw(){
+  draw() {
     ctx.beginPath();
-    ctx.drawImage(img1,this.x,this.y,this.size,this.size);
-    // ctx.arc(this.x,this.y,50,0,Math.PI*2);
+    ctx.drawImage(img1, this.x, this.y, this.size, this.size);
     ctx.fillStyle = '#fff';
-    ctx.fillRect(this.x + this.size / 2 -1,this.y + this.size / 2,2,this.size*2);
+    ctx.fillRect(this.x + this.size / 2 - 1, this.y + this.size / 2, 2, this.size * 1.5);
     ctx.closePath();
     ctx.fill();
   }
@@ -45,20 +44,20 @@ class Baloon{
 let baloon = new Baloon();
 baloon.draw();
 
-window.onresize = function(){
+window.onresize = function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 };
 
 function update() {
   ctx.fillStyle = '#E2E3E3';
-  ctx.rect(0,0,canvas.width,canvas.height);
+  ctx.rect(0, 0, canvas.width, canvas.height);
   ctx.fill();
   baloon.move();
   baloon.draw();
-  setTimeout(function(){
+  setTimeout(function() {
     update();
-  },30);
+  }, 30);
 }
 
 update();
