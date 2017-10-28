6ecfa4cfdbde9c26ca26c133ade0fb5430c0a752
sKawashima <bysk.333@gmail.com>
2017-10-28 16:22:19 +0900

    topviewだいたい完成, 背景にcanvasを敷いた

---
+++ dest/js/main.js
@@ -0,0 +1,1 @@
+var canvas=document.getElementById("canvas");canvas.width=800,canvas.height=600;var ctx=canvas.getContext("2d");ctx.beginPath(),ctx.fillStyle="#E2E3E3",ctx.rect(0,0,canvas.width,canvas.height),ctx.fill();var Baloon=function(){return function(){}}();
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,15 @@
-//console.log('hello');
+const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+const ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.fillStyle = "#E2E3E3";
+ctx.rect(0,0,canvas.width,canvas.height);
+ctx.fill();
+
+class Baloon{
+  private x: number;
+  private y: number;
+  private shape: string;
+  
+}
