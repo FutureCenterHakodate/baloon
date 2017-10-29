775fe4a1466331dc7188aac4e202364d3708e820
ryu-g <b1013178@fun.ac.jp>
2017-10-29 23:03:40 +0900

    第一次移植完了

--- page-temp.php
+++ page-temp.php
@@ -6,17 +6,17 @@ Template Name: for baloon
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-    <link rel="stylesheet" type="text/css" href=<?php echo get_template_directory_uri(); ?>/dest/css/style.css>
+    <link rel="stylesheet" type="text/css" href=<?php echo get_stylesheet_directory_uri(); ?>/dest/css/style.css>
     <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet">
   </head>
   <body>
     <canvas id="canvas">このページはCanvasを使用しています。</canvas>
     <div id="topview">
       <div class="cartoon">
-      <img class="item" src=<?php echo get_template_directory_uri(); ?>dest/img/baloon01.jpg>
-      <img class="item" src=<?php echo get_template_directory_uri(); ?>dest/img/baloon02.jpg>
-      <img class="item" src=<?php echo get_template_directory_uri(); ?>dest/img/baloon03.jpg>
-      <img class="item" src=<?php echo get_template_directory_uri(); ?>dest/img/baloon04.jpg></div>
+      <img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon01.jpg>
+      <img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon02.jpg>
+      <img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon03.jpg>
+      <img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon04.jpg></div>
       <h1>ふうせんの森</h1>
       <p class="subTitle">
         ちょうどよく、うかぶ。<br>きもちよく、うかぶ。</p>
@@ -97,7 +97,11 @@ Template Name: for baloon
         <p>&copy;深沢アート研究所</p>
       </div>
     </footer>
+    <script>
+    var path = "<?php echo get_template_directory_uri();?>";
+    </script>
     <script>(function() {'use strict';})();</script>
     <script src="js/main.js"></script>
   </body>
+
 </html>
