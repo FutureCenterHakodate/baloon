c457b47aada0be770f1cceda5d3e253ea45689a1
ryu-g <b1013178@fun.ac.jp>
2017-10-29 11:54:11 +0900

    transplanting in php
    jsから呼び出すsvg画像へのパスが通ってないっぽい
    php側から呼び出すものに関してはget_templete_directory_urtを噛ませて画像とcssが呼び出せています

--- page-baloon.php
+++ page-baloon.php
@@ -5,15 +5,23 @@ Template Name: 固定ページの新規テンプレート１
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-	<title><?php wp_title("- はこだてみらい館",true,'right');?></title>	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href=<?php echo get_template_directory_uri();?>/css/baloon.css>
-	<link rel="icon" href=<?php echo get_template_directory_uri();?>/img/favicon.ico>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
-</head>
-<body>
-<iframe src="https://www.google.com/maps/d/embed?mid=1mUylVVj0RMzRIvoalkNCDq93P-M" width="100%" height="400"></iframe>
-
-</body>
+  <head>
+    <link rel="stylesheet" type="text/css" href=<?php echo get_template_directory_uri(); ?>/dest/css/style.css>
+    <link href="https://fonts.googleapis.com/earlyaccess/roundedmplus1c.css" rel="stylesheet">
+  </head>
+  <body>
+    <canvas id="canvas">このページはCanvasを使用しています。</canvas>
+    <div id="topview">
+      <div class="cartoon"><img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon01.jpg><img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon02.jpg><img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon03.jpg><img class="item" src=<?php echo get_template_directory_uri(); ?>/dest/img/baloon04.jpg></div>
+      <h1>ふうせんの森</h1>
+      <p class="subTitle">
+        ちょうどよく、うかぶ。<br>きもちよく、うかぶ。</p>
+      <div class="poster">
+        <p class="date">11月3日〜12月10日</p>
+        <p class="place">はこだてみらい館</p>
+      </div>
+    </div>
+    <script>(function() {'use strict';})();</script>
+    <script src=<?php echo get_template_directory_uri(); ?>/dest/js/main.js></script>
+  </body>
 </html>
