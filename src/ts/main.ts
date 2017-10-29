a377c4feedebc52dfdb6930ebb50fcffab30548a
sKawashima <bysk.333@gmail.com>
2017-10-30 00:04:43 +0900

    js try it

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -10,14 +10,16 @@ const img0 = new Image();
 const img1 = new Image();
 const img2 = new Image();
 
+var path = document.getElementById( 'mainjs' ).getAttribute( 'path' );
+
 try{
   img0.src = '../img/baloon-ellipse.png';
   img1.src = '../img/baloon-heart.png';
   img2.src = '../img/baloon-star.png';
 }catch(err){
-  img0.src = path + '/img/baloon-ellipse.png';
-  img1.src = path + '/img/baloon-heart.png';
-  img2.src = path + '/img/baloon-star.png';
+  img0.src = path + '/dest/img/baloon-ellipse.png';
+  img1.src = path + '/dest/img/baloon-heart.png';
+  img2.src = path + '/dest/img/baloon-star.png';
 }
 class Baloon {
   private x: number;
