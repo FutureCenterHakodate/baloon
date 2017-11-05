e657618a8138224537cbe8e33707a8f10db0fde3
ryu-g <b1013178@fun.ac.jp>
2017-11-05 12:07:36 +0900

    fixed page-baloon

--- page-baloon.php
+++ page-baloon.php
@@ -56,8 +56,8 @@ Template Name: for baloon
         <p>ここには、みんながつくった「ちょうどいい」が集められます</p>
         <div class="flex-pic-container">
           <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/001.jpg></div>
-          <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/001.jpg></div>
-          <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/001.jpg></div>
+          <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/002.jpg></div>
+          <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/003.jpg></div>
           <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/001.jpg></div>
           <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/001.jpg></div>
           <div class="flex-pic-item"><img src=<?php echo get_template_directory_uri(); ?>/baloon-img/activity/001.jpg></div>
