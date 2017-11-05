4a01ad5ed62767348546ef294dc965639ae44296
sKawashima <bysk.333@gmail.com>
2017-11-05 13:57:06 +0900

    apply atom beauty

--- src/ts/modal.ts
+++ src/ts/modal.ts
@@ -1,25 +1,23 @@
 let bImage = document.getElementsByClassName('flex-pic-item');
 
-// console.log(bImage[0].children[0]);
-
-for(let i = 0;i < bImage.length;i++){
-  bImage[i].children[0].setAttribute('onclick',"cl(this.getAttribute('src'))");
+for (let i = 0; i < bImage.length; i++) {
+  bImage[i].children[0].setAttribute('onclick', "cl(this.getAttribute('src'))");
 }
 
-var cl = function(data){
+var cl = function(data) {
   modalToggle();
-  modal.children[1].setAttribute('src',data)
+  modal.children[1].setAttribute('src', data)
 };
 
 //以下、モーダルウィンドウ内の制御
 
 let modal = document.getElementById('modal');
 
-modal.onclick = function(){
+modal.onclick = function() {
   modalToggle();
 }
 
 const modalToggle = function toggleStyle() {
-    modal.classList.toggle('modal-hide');
-    modal.classList.toggle('modal-display');
+  modal.classList.toggle('modal-hide');
+  modal.classList.toggle('modal-display');
 }
